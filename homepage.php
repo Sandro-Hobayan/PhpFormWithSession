d7a5317af2c9fe615550b401f1ad0b58a709d93e
Alesandro Hobayan <alesandrosfboo@gmail.com>
2025-04-14 18:41:20 +0800

    second commit feat: loginpage done, registerpage done, homepage wip

--- homepage.php
+++ homepage.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Homepage</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
   <?php
@@ -25,7 +27,7 @@
   }
   ?>
   <div class="proContainer">
-    <image
+    <img src="images/2x2.jpg" class="rounded-circle" alt="Cinque Terre" width="304" height="236"> 
   </div>
 </body>
 </html>
